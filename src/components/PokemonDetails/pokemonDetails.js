387c82daca2e53b3fc45cc4fd7dfa6d6d45ee2ce
Roman <roman.g.demyanchuk@gmail.com>
2020-05-23 19:46:51 +0300

    Preloader

--- src/components/PokemonDetails/pokemonDetails.js
+++ src/components/PokemonDetails/pokemonDetails.js
@@ -39,7 +39,7 @@ export default class PokemonDetails extends Component {
         error: false
     };
     onError = () => {
-        this.setState({error: true, loading: true})
+        this.setState({error: true, loading: false})
     };
     constructor(props) {
         super(props);
@@ -56,7 +56,7 @@ export default class PokemonDetails extends Component {
             .then((pokemon) => {
                 this.setState({
                     pokemon,
-                    loading: true
+                    loading: false
                 }, () => {
                     this.checkPokemonIsFavorite();
                 });
