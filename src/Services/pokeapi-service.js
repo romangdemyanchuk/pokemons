50c723a379537c1d79f756a8b08fba207752b4f3
Roman <roman.g.demyancuk@gmail.com>
2020-05-25 11:32:49 +0300

    add log

--- src/Services/pokeapi-service.js
+++ src/Services/pokeapi-service.js
@@ -1,6 +1,7 @@
 export default class PokeApiService {
     _apiBase = 'https://pokeapi.co/api';
     async getResource(url) {
+        console.log(`${this._apiBase}${url}`)
         const  res = await fetch(`${this._apiBase}${url}`);
         if (!res.ok) {
             throw new Error(`Could not fetch ${url} received ${res.status}`)
